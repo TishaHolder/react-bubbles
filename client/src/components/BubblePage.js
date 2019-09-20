45433736224fc48d548ee17c83093b0c25cafb73
Tisha Holder <50963507+TishaHolder@users.noreply.github.com>
2019-09-20 11:52:53 -0400

    fetching color data for bubbles

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,14 +1,35 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { axiosWithAuth } from "../axiosWithAuth.js";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
 const BubblePage = () => {
+
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
 
+
+  //THE AXIOS CALL IN THIS useEffect populates the empty array here: const [colorList, setColorList] = useState([]);
+  useEffect( () => {
+
+    {/*GOT THIS ERROR: TypeError: _axiosWithAuth_js__WEBPACK_IMPORTED_MODULE_3__.axiosWithAuth.get is not a function
+    FIXED IT BY ADDING PARENTHESES AFTER axiosWithAuth(). Before I had axiosWwithAuth.get */}
+
+    axiosWithAuth().get("http://localhost:5000/api/colors")
+    .then(res => {
+      console.log("get color data", res.data);
+      setColorList(res.data);
+    })
+    .catch(err => {
+      console.log(err.response);
+    })
+
+  }, []); 
+
+  
   return (
     <>
       <ColorList colors={colorList} updateColors={setColorList} />
